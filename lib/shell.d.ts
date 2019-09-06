8274034cec2b8a656c8d6bc4599271de7239265d
Toru Nagashima <public@mysticatea.dev>
2019-09-06 15:56:50 +0900

    🔖 0.0.0-alpha.1 (built with 509a7a0)

---
+++ lib/shell.d.ts
@@ -0,0 +1,4 @@
+export declare function cd(path: string): void;
+export declare function sh(command: string): void;
+export declare function test(command: string): boolean;
+export declare function rmrf(glob: string): void;
