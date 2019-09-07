373dc98959e924a145ad0e0cfc00cda19cfb40f0
Toru Nagashima <public@mysticatea.dev>
2019-09-07 21:47:19 +0900

    🔖 0.0.1 (built with e4eb8c1)

--- lib/shell.d.ts
+++ lib/shell.d.ts
@@ -1,4 +1,4 @@
 export declare function cd(path: string): void;
-export declare function sh(command: string): void;
-export declare function test(command: string): boolean;
-export declare function rmrf(glob: string): void;
+export declare function git(args: string): Promise<void>;
+export declare function testGit(args: string): Promise<boolean>;
+export declare function rmrf(glob: string): Promise<void>;
---
+++ lib/url.d.ts
@@ -0,0 +1,2 @@
+export declare let getUrl: (repo: string, user: string, password: string) => string;
+export declare function setGetUrl(f: typeof getUrl): void;
